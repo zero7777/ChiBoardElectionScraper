dff0cc848f276fecbda1f339ae98fe243d27915e
grygoree <GregoryLeung2016@u.northwestern.edu>
2016-05-03 08:44:26 -0500

    updated scraper to warn when request fails.  Also set up skeleton to scrape from URL directly

--- scraper.py
+++ scraper.py
@@ -10,15 +10,23 @@ def scrapeWardElection(elec_code, ward, race_number):
     url = "http://www.chicagoelections.com/en/pctlevel3.asp?Ward=" + str(ward) + "&elec_code=" + str(elec_code) + "&race_number=" + str(race_number)
 
     response = requests.get(url) #todo check dis function to be safe
-    print url
-    print response.text
+
+    if response.status_code != 200:
+        print "Failed to scrape from " + url
+    #print response.text
     return response.text
 
-def scrapeElection(elec_code):
+def scrapeElectionURL(url):
+    response = requests.post(url, data=payload)
+    if response.status_code != 200:
+        print "Failed to scrape from " + url
+    return response.text
+
+def scrapeElection(elec_code,contest):
     #According to the website, the form takes a POST request to GET data. WutFace
     payload = {
         'VTI-GROUP': 0,
-        'D3':        'President, U.S.',
+        'D3':        contest,
         'flag':      1,
         'B1':        'View The Results'
     }
@@ -54,11 +62,12 @@ def convertHTMLToCSV(html,file_name):
     #MOTHER FUCKING LIST COMPREHENSION WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
     rows = [[c.get_text() for c in r.find_all("td")] for r in html_rows]
 
+    #this will break if there are less than 4 rows in the table lol
     writeCSVRows(rows[3:-2],file_name)
     return
 
 if __name__ == "__main__":
-    testText = scrapeElection("5")
+    testText = scrapeElection("5","President, U.S.")
     convertHTMLToCSV(testText, "election.csv")
     getText = scrapeWardElection(5,1,10)
     convertHTMLToCSV(getText,"ward.csv")
